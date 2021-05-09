775aa520e76524ab46470e79f8855ca0ba6e63c5
Fannese <manuelafannese51@yahoo.com>
2021-05-09 20:25:08 +0200

    first commit

---
+++ JavaScriptbeispiel.js
@@ -0,0 +1,13 @@
+﻿let onclickbutton = document.querySelector('#Click-me');
+onclickbutton.addEventListener('click', function () {
+    let input = document.querySelector('#input');
+    let login = document.querySelector('#login_box');
+    let firstname = document.querySelector('#Firstname');
+    let newDivFirsname = document.createElement('div');
+    //let newContent = document.createTextNode(firstname.value);
+    let inputfeld = document.createTextNode(input.value);
+    newDivFirsname.appendChild(inputfeld);
+
+})
+
+
